8c7f327884ac230a56eafa68be7fc1290c5b4bdc
Marsoupilami25 <marsoupilami25@free.fr>
2025-08-29 09:45:20 +0200

    manage escape comment

--- src/hakai_packages/knx_utils/__init__.py
+++ src/hakai_packages/knx_utils/__init__.py
@@ -1,3 +1,3 @@
 from .serializable import Serializable, Quoted, serializable_to_yaml
-from .knx_tools import knx_transformed_string, knx_flat_string
+from .knx_tools import knx_transformed_string, knx_flat_string, knx_update_comment_list
 from.knx_named_class import KNXNamedClass
