c60e85fd0c604ed2dbf7e1a4caa2d395835c6c63
Marsoupilami25 <marsoupilami25@free.fr>
2025-08-30 19:22:53 +0200

    version 1.2

--- src/hakai_packages/knx_utils/__init__.py
+++ src/hakai_packages/knx_utils/__init__.py
@@ -3,4 +3,4 @@ from .knx_tools import (knx_transformed_string, knx_flat_string,
                         knx_update_comment_list, knx_build_string)
 from.knx_named_class import KNXNamedClass
 
-__version__ = "1.1"
+__version__ = "1.2"
