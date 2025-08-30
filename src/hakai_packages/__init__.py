8b91f1e57226a27cd7a3c7e2d17ecded482a1045
Marsoupilami25 <marsoupilami25@free.fr>
2025-08-30 18:25:29 +0200

    add header in generated files

--- src/hakai_packages/__init__.py
+++ src/hakai_packages/__init__.py
@@ -1,5 +1,4 @@
 from .knx_function_analyzer import HAKNXLocationsRepository, KNXSpaceAnalyzer
 from .knx_project import KNXProjectManager
 from .hakai_conf import HAKAIConfiguration
-
-__version__ = "1.1"
+from .knx_utils import __version__
